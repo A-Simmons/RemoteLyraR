9b6e2d618a11f938a03a0e68b7e62d6947dd2efa
Alex <alex.simmons.1992@gmail.com>
2016-06-22 12:24:09 +1000

    Modified install.cygwin

--- R/installCygwin.R
+++ R/installCygwin.R
@@ -1,4 +1,6 @@
 installCygwin.windows<-function() {
   devtools::use_package("installr", "Suggests")
-  success<-install.Cygwin(bit = 32, installed_option="-P dos2unix,expect,sshOpen")
+  if (requireNamespace("installer", quietly = TRUE)) {
+    installer::f()
+  }
 }
