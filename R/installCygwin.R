f0a98e7388cb55abd3b0684ce31b7214c6ed3cd5
Alex <alex.simmons.1992@gmail.com>
2016-06-22 12:17:59 +1000

    More installr package changes

--- R/installCygwin.R
+++ R/installCygwin.R
@@ -1,5 +1,4 @@
 installCygwin.windows<-function() {
   devtools::use_package("installr", "Suggests")
-  requireNamespace("installr")
   success<-install.Cygwin(bit = 32, installed_option="-P dos2unix,expect,sshOpen")
 }
